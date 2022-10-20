0be594c88772ab2a3413f44eea37f46983d18646
Keks <keks@htmlacademy.ru>
2022-10-20 20:41:50 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/thumbnails.js
@@ -0,0 +1,26 @@
+import {createPhotoMocksArray} from './photo-mocks.js';
+import {openPicturePreview} from './big-picture-preview.js';
+
+const picsGalleryElement = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const photoMocks = createPhotoMocksArray();
+const picturesGalleryFragment = document.createDocumentFragment();
+
+photoMocks.forEach((mock) => {
+  const picture = pictureTemplate.cloneNode(true);
+  const {url, likes, comments} = mock;
+
+  picture.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openPicturePreview(mock);
+  });
+
+  picture.querySelector('.picture__img').src = url;
+  picture.querySelector('.picture__likes').textContent = likes;
+  picture.querySelector('.picture__comments').textContent = comments.length;
+
+  picturesGalleryFragment.append(picture);
+});
+
+picsGalleryElement.append(picturesGalleryFragment);
